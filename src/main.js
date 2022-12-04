b0695422cf70be6d1bb3901639e13d7540b8f497
alweismiau <keizafina@gmail.com>
2022-12-04 19:56:14 +0700

    bootstrap

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from "./router";
 
-// import 'flowbite';
-
-// import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const pinia = createPinia()
 const app = createApp(App)
